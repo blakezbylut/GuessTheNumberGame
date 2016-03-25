920112ca63f5cd7d4b2c4be973856fc2e90a4208
Blake Zbylut <blakezbylut@gmail.com>
2016-03-25 13:22:52 -0400

    adds guess the number changes

---
+++ scripts.js
@@ -0,0 +1,23 @@
+$(function(){
+
+  var answer = parseInt(Math.floor(Math.random()*10));
+
+  var userInput = parseInt(prompt("Enter a number, stranger!"));
+
+var something = function() {
+  alert("Welcome");
+  // while( userInput > answer ) {
+  //   alert("Guess a bit lower, matey");
+  //   return userInput;
+  // };
+  // while( userInput < answer ) {
+  //   alert("Guess a bit higher, mate");
+  //   return userInput;
+  // };
+  // while (userInput == answer) {
+  //   return $('<h1>').text('You win!');
+  // }
+};
+something();
+
+})
